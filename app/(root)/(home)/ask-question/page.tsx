0abb10d32af87b454185abe1c954d522f219b941
Rupie17 <johnruperto17@gmail.com>
2023-10-15 16:57:24 -0700

    Ask a question page front end functionality.

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -1,9 +1,13 @@
+import Question from '@/components/forms/Question'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
-      <Question />
+      <h1 className='h1-bold text-dark100_light900'>Ask A Question</h1>
+      <div className='mt-9'>
+        <Question />
+      </div>
     </div>
   )
 }
